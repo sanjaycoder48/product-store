1e2c22db39c951fcd330e0376cf01ae9ee03e7c0
sanjay E <sanjaye0408@gmail.com>
2026-01-05 23:14:52 +0530

    basic file installation

---
+++ Backend/controllers/productController.js
@@ -0,0 +1,27 @@
+import pool from '../db.js';
+
+export const getProducts = async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM products');
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+export const getProductById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query('SELECT * FROM products WHERE id = $1', [id]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
